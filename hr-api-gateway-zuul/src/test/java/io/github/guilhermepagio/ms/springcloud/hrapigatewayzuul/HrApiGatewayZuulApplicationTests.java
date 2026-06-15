6541e131f1f15419cd2f650f0a83baef5d551c22
guilhermepagio <guilhermepagio.contato@gmail.com>
2026-06-15 03:17:43 -0300

    feat: secao 3 15/06/2026 03:17

---
+++ hr-api-gateway-zuul/src/test/java/io/github/guilhermepagio/ms/springcloud/hrapigatewayzuul/HrApiGatewayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.guilhermepagio.ms.springcloud.hrapigatewayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrApiGatewayZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
