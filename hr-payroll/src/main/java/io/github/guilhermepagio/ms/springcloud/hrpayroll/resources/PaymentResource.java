cbea2de7a33b12a4093bb3b3b7f590e5d4afb3d8
guilhermepagio <guilhermepagio.contato@gmail.com>
2026-06-15 02:21:04 -0300

    feat: secao 3 15/06/2026 02:20

--- hr-payroll/src/main/java/io/github/guilhermepagio/ms/springcloud/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/io/github/guilhermepagio/ms/springcloud/hrpayroll/resources/PaymentResource.java
@@ -1,13 +1,15 @@
 package io.github.guilhermepagio.ms.springcloud.hrpayroll.resources;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 import io.github.guilhermepagio.ms.springcloud.hrpayroll.entities.Payment;
 import io.github.guilhermepagio.ms.springcloud.hrpayroll.services.PaymentService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 @RequestMapping(value = "/payments")
@@ -19,9 +21,15 @@ public class PaymentResource {
         this.paymentService = paymentService;
     }
 
+    @HystrixCommand(fallbackMethod = "getPaymentFallback")
     @GetMapping("/{idWorker}/days/{qtdDays}")
     public ResponseEntity<Payment> getMethodName(@PathVariable Long idWorker, @PathVariable Integer qtdDays) {
         final Payment payment = paymentService.getPayment(idWorker, qtdDays);
+        return ResponseEntity.ok(payment);
+    }
+
+    public ResponseEntity<Payment> getPaymentFallback(Long idWorker, Integer qtdDays) {
+        final Payment payment = new Payment("Brann", 400.0, qtdDays);
         return ResponseEntity.ok(payment);
     }
 
