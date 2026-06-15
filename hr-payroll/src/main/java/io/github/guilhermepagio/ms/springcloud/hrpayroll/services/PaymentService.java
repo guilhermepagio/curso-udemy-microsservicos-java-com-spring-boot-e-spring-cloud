90fd1c25012e25a420d77c98ff8369ccab0e555d
guilhermepagio <guilhermepagio.contato@gmail.com>
2026-06-15 11:45:51 -0300

    feat: secao 3 15/06/2026 11:45

--- hr-payroll/src/main/java/io/github/guilhermepagio/ms/springcloud/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/io/github/guilhermepagio/ms/springcloud/hrpayroll/services/PaymentService.java
@@ -1,13 +1,9 @@
 package io.github.guilhermepagio.ms.springcloud.hrpayroll.services;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import org.springframework.stereotype.Service;
-
 import io.github.guilhermepagio.ms.springcloud.hrpayroll.entities.Payment;
 import io.github.guilhermepagio.ms.springcloud.hrpayroll.entities.Worker;
 import io.github.guilhermepagio.ms.springcloud.hrpayroll.feignclients.WorkerFeignClient;
+import org.springframework.stereotype.Service;
 
 @Service
 public class PaymentService {
@@ -19,9 +15,6 @@ public class PaymentService {
     }
 
     public Payment getPayment(Long workerId, Integer days) {
-        final Map<String, String> uriVariables = new HashMap<>();
-        uriVariables.put("id", workerId.toString());
-
         final Worker worker = workerFeignClient.findById(workerId).getBody();
 
         if (worker == null) {
