69f54cb778085f40dcf550d6ee69d957be7eb400
guilhermepagio <guilhermepagio.contato@gmail.com>
2026-07-03 16:19:50 -0300

    feat: secao 03/07/2026 16:19

---
+++ hr-oauth/src/main/java/io/github/guilhermepagio/ms/springcloud/hroauth/entities/Role.java
@@ -0,0 +1,31 @@
+package io.github.guilhermepagio.ms.springcloud.hroauth.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class Role {
+
+    private Long id;
+    private String roleName;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+
+        Role role = (Role) o;
+
+        return roleName != null ? roleName.equals(role.roleName) : role.roleName == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return roleName != null ? roleName.hashCode() : 0;
+    }
+}
---
+++ hr-oauth/src/main/java/io/github/guilhermepagio/ms/springcloud/hroauth/entities/User.java
@@ -0,0 +1,40 @@
+package io.github.guilhermepagio.ms.springcloud.hroauth.entities;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    private Long id;
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    private Set<Role> roles = new HashSet<>();
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+
+        User user = (User) o;
+
+        return id != null ? id.equals(user.id) : user.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+}
